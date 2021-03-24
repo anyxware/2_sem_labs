c9bfc4a88089cfc0ab67d95effc16e805ef5c0dc
anyxware <parrot@localhost.localdomain>
2021-03-24 22:57:45 +0300

    commit

--- 3lab/wmain.c
+++ 3lab/wmain.c
@@ -30,13 +30,13 @@ char* get_str(){
 }
 
 int isPrime(int n){
-    if (n > 1){
-        for (int i = 2; i < n; i++)
-            if (n % i == 0)
-                return 0;
-        return 1;
-    }else
-        return 0;
+	if (n > 1){
+		for (int i = 2; i < n; i++)
+			if (n % i == 0)
+				return 0;
+		return 1;
+	}else
+		return 0;
 }
 
 void error(){
